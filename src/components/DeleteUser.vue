37a48d5a62e206a1f94ceafd2d2783163e2864a5
Supakit <supakit.pnm@gmail.com>
2022-06-26 22:09:55 +0700

    Add: Local Component Mixin

--- src/components/DeleteUser.vue
+++ src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="errorTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -10,6 +10,7 @@
 
 <script>
 import UserAlert from './UserAlert.vue';
+import AlertMixin from '../mixins/alert.js';
 
 export default {
   components: {
@@ -17,16 +18,9 @@ export default {
   },
   data() {
     return {
-      alertIsVisible: false,
+      errorTitle: 'Error ?',
     };
   },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
-  },
+  mixins: [AlertMixin],
 };
 </script>
